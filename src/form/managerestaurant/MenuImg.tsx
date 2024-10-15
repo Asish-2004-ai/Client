bfe74cf9783b9463809626f1cbac2659d070cad3
Asish-2004-ai <asishdalabehera375@gmail.com>
2024-10-15 08:54:09 +0530

    changed

--- src/form/managerestaurant/MenuImg.tsx
+++ src/form/managerestaurant/MenuImg.tsx
@@ -1,4 +1,3 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useFormContext } from "react-hook-form"
@@ -13,11 +12,10 @@ const MenuImg = () => {
         <h2 className="font-bold text-2xl">Image</h2>
         <FormDescription>Upload an image to showcase your restaurant.</FormDescription>
       </div>
-      <div className="flex flex-col md:w-[50%]">
+      <div className="flex flex-col md:w-[60%]">
         {existImgUrl && (
-          <AspectRatio ratio={14 / 16}>
-            <img src={existImgUrl} className='object-cover h-[300px] md:h-[400px] w-full rounded-md' alt="" />
-          </AspectRatio>
+            <img src={existImgUrl} className='object-cover sm:h-[150px] h-[300px] md:h-[400px] w-full rounded-md' alt="" />
+          
         )}
         <FormField  control={control} name="imageFile" render={({ field }) => (
           <FormItem className=''>
